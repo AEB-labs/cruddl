c5a38acc6cf921421dc5dcc9a81b99b85ae14fa0
Jan Melcher <jan.melcher@aeb.com>
2017-09-23 16:37:36 +0200

    Adjust query-tree-builder spec
    EntitiesQueryNode now no longer does the filtering etc.

--- spec/query/query-tree-builder.spec.ts
+++ spec/query/query-tree-builder.spec.ts
@@ -2,7 +2,7 @@ import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString
 import { distillQuery } from '../../src/graphql/query-distiller';
 import { createQueryTree } from '../../src/query/query-tree-builder';
 import any = jasmine.any;
-import { EntitiesQueryNode, FieldQueryNode, ObjectQueryNode } from '../../src/query/definition';
+import { EntitiesQueryNode, FieldQueryNode, ListQueryNode, ObjectQueryNode } from '../../src/query/definition';
 import objectContaining = jasmine.objectContaining;
 
 describe('query-tree-builder', () => {
@@ -36,11 +36,13 @@ describe('query-tree-builder', () => {
         const queryTree = createQueryTree(op);
         expect(queryTree.properties.length).toBe(1);
         expect(queryTree.properties[0].propertyName).toBe('allUsers');
-        expect(queryTree.properties[0].valueNode).toEqual(any(EntitiesQueryNode));
-        const entitiesNode = queryTree.properties[0].valueNode as EntitiesQueryNode;
+        expect(queryTree.properties[0].valueNode).toEqual(any(ListQueryNode));
+        const listNode = queryTree.properties[0].valueNode as ListQueryNode;
+        expect(listNode.listNode).toEqual(any(EntitiesQueryNode));
+        const entitiesNode = listNode.listNode as EntitiesQueryNode;
         expect(entitiesNode.objectType).toBe(userType);
-        expect(entitiesNode.innerNode).toEqual(any(ObjectQueryNode));
-        const objectNode = entitiesNode.innerNode as ObjectQueryNode;
+        expect(listNode.innerNode).toEqual(any(ObjectQueryNode));
+        const objectNode = listNode.innerNode as ObjectQueryNode;
         expect(objectNode.properties.length).toBe(2);
         expect(objectNode.properties[0].propertyName).toBe('code');
         expect(objectNode.properties[0].valueNode).toEqual(any(FieldQueryNode));
