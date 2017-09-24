71602fe55f0adc56a5d8e67a33d1c78cfc7e7c24
Sebastian Henke <s.henke@henke-informatik.de>
2017-09-24 16:40:25 +0200

    Add transformer which adds scalar types like DateTime.

---
+++ src/schema/preparation/ast-transformation-modules/add-scalar-types.ts
@@ -0,0 +1,15 @@
+import {ASTTransformer} from "../ast-transformer";
+import {DocumentNode, ScalarTypeDefinitionNode} from "graphql";
+import {NAME, SCALAR_TYPE_DEFINITION} from "graphql/language/kinds";
+import {SCALAR_DATE, SCALAR_DATETIME, SCALAR_TIME} from "../../schema-defaults";
+import {buildScalarDefinitionNode} from "../../schema-utils";
+
+export class AddScalarTypesTransformer implements ASTTransformer {
+
+    transform(ast: DocumentNode): void {
+        ast.definitions.push(buildScalarDefinitionNode(SCALAR_DATETIME));
+        ast.definitions.push(buildScalarDefinitionNode(SCALAR_DATE));
+        ast.definitions.push(buildScalarDefinitionNode(SCALAR_TIME));
+    }
+
+}
