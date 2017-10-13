2d20d386a36be49de33e7f8f24d322e1765db6e1
Jan Melcher <jan.melcher@aeb.com>
2017-10-13 20:31:12 +0200

    Enable colors in dev server

--- spec/dev/start.ts
+++ spec/dev/start.ts
@@ -3,6 +3,9 @@ import {start} from "./server";
 // to get through firewall
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+const colors = require('colors');
+colors.enabled = true;
+
 start().catch(error => {
     console.error(error.stack);
 });
