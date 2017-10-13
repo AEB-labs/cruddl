546a61e92811746ae114ffe54403923966da7986
Frederik Niehus <frederik.niehus@aeb.com>
2017-10-13 19:33:49 +0200

    Export createSchema in index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export { createSchema } from './src/schema/schema-builder';
 export { addQueryResolvers } from './src/query/query-resolvers';
 export { DatabaseAdapter } from './src/database/database-adapter';
 export * from './src/database/arangodb';
