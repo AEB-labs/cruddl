5713ceda672cc01008d9d1eb2691023529d1b8ff
Jan Melcher <jan.melcher@aeb.com>
2017-10-14 21:54:08 +0200

    Print stack trace in resolver error

--- src/query/query-resolvers.ts
+++ src/query/query-resolvers.ts
@@ -6,13 +6,18 @@ import { createQueryTree } from './query-tree-builder';
 
 export function addQueryResolvers(schema: GraphQLSchema, databaseAdapter: DatabaseAdapter) {
     return addOperationBasedResolvers(schema, async operationInfo => {
-        console.log(print(operationInfo.operation));
-        const op = distillOperation(operationInfo);
-        console.log(op.describe());
-        const queryTree = createQueryTree(op);
-        console.log(queryTree.describe());
-        const result = await databaseAdapter.execute(queryTree);
-        console.log(result);
-        return result;
+        try {
+            console.log(print(operationInfo.operation));
+            const op = distillOperation(operationInfo);
+            console.log(op.describe());
+            const queryTree = createQueryTree(op);
+            console.log(queryTree.describe());
+            const result = await databaseAdapter.execute(queryTree);
+            console.log(JSON.stringify(result, undefined, '  '));
+            return result;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
     });
 }
