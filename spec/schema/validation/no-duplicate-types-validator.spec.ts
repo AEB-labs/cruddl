16a0cc2428ec0c3667f6b400533cf012e1bb4058
Sebastian Henke <s.henke@henke-informatik.de>
2017-10-18 14:27:59 +0200

    Run only the validator to test (instead of all validators)

--- spec/schema/validation/no-duplicate-types-validator.spec.ts
+++ spec/schema/validation/no-duplicate-types-validator.spec.ts
@@ -1,5 +1,6 @@
-import {validateModel} from "../../../src/schema/preparation/ast-validator";
+import {validateModel, ValidationResult} from "../../../src/schema/preparation/ast-validator";
 import {parse} from "graphql";
+import {NoDuplicateTypesValidator} from "../../../src/schema/preparation/ast-validation-modules/no-duplicate-types-validator";
 
 const modelWithoutDuplicates = `
             type Stuff {
@@ -19,13 +20,13 @@ const modelWithDuplicate = `
 describe('no duplicate type definition validator', () => {
     it('finds duplicate types', () => {
         const ast = parse(modelWithDuplicate);
-        const validationResult = validateModel(ast);
+        const validationResult = new ValidationResult(new NoDuplicateTypesValidator().validate(ast));
         expect(validationResult.hasErrors()).toBeTruthy();
     });
 
     it('ignores unique types', () => {
         const ast = parse(modelWithoutDuplicates);
-        const validationResult = validateModel(ast);
+        const validationResult = new ValidationResult(new NoDuplicateTypesValidator().validate(ast));
         expect(validationResult.hasErrors()).toBeFalsy();
     })
 
