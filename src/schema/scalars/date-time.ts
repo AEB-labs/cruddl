b98fda20956e480058ae36bae4797ba6e288b7ac
Jan Melcher <jan.melcher@aeb.com>
2017-10-20 15:22:48 +0200

    Provide serialization, ... functions for DateTime scalar

---
+++ src/schema/scalars/date-time.ts
@@ -0,0 +1,30 @@
+import { GraphQLScalarType } from 'graphql';
+import moment = require('moment');
+
+function isValidDateTime(value: any) {
+    return moment(value, moment.ISO_8601, true).isValid();
+}
+
+function coerceDateTime(value: any): string {
+    if (!isValidDateTime(value)) {
+        throw new TypeError(`Invalid ISO 8601 DateTime: ${value}`);
+    }
+    return value;
+}
+
+export const GraphQLDateTime = new GraphQLScalarType({
+    name: 'DateTime',
+    description:
+    'The `DateTime` scalar type represents a point in time with an optional timezone specifier, in a format specified by ISO 8601.',
+    serialize: coerceDateTime,
+    parseValue: coerceDateTime,
+    parseLiteral(ast) {
+        if (ast.kind === 'StringValue') {
+            const value = ast.value;
+            if (isValidDateTime(value)) {
+                return value;
+            }
+        }
+        return null;
+    }
+});
--- src/schema/schema-builder.ts
+++ src/schema/schema-builder.ts
@@ -1,21 +1,25 @@
 import {buildASTSchema, DocumentNode, GraphQLSchema, parse, print, Source} from "graphql";
 import {transformModel} from "./preparation/ast-transformer";
 import {validateModel} from "./preparation/ast-validator";
+import { implementScalarTypes } from './scalars/implement-scalar-types';
 
 /*
  Create a schema for a given model definition. A model definition is an array of definition parts, represented as a (sourced) SDL string or AST document.
   */
 export function createSchema(modelDefinition: Array<Source | DocumentNode>): GraphQLSchema {
     const modelAST = mergeModelDefinition(modelDefinition);
+
     const validationResult = validateModel(modelAST);
     if(validationResult.hasErrors()) {
-        throw new Error('Invalid model: ' + validationResult.messages.forEach(msg => msg.toString()))
+        throw new Error('Invalid model:\n' + validationResult.messages.map(msg => msg.toString()).join('\n'))
     } else {
         console.log('Thank you for your valid model. I will now do some magic with it.')
     }
+
     const schemaAST = transformModel(modelAST);
     console.log(print(schemaAST));
-    return buildASTSchema(schemaAST);
+    const schema = buildASTSchema(schemaAST);
+    return implementScalarTypes(schema);
 }
 
 function mergeModelDefinition(modelDefinitions: Array<Source | DocumentNode>): DocumentNode {
