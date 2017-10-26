f4262f3a0284ff6c1ffa212b93ff627cbdf6db32
Sebastian Henke <s.henke@henke-informatik.de>
2017-10-26 14:02:22 +0200

    Distinguish between entity kinds and get error or warning

--- spec/schema/validation/no-empty-object-types-validator.spec.ts
+++ spec/schema/validation/no-empty-object-types-validator.spec.ts
@@ -3,7 +3,7 @@ import {parse} from "graphql";
 import {NoDuplicateTypesValidator} from "../../../src/schema/preparation/ast-validation-modules/no-duplicate-types-validator";
 import {
     NoEmptyObjectTypesValidator,
-    VALIDATION_WARNING_OBJECT_TYPE_WITHOUT_FIELDS
+    VALIDATION_ERROR_OBJECT_TYPE_WITHOUT_FIELDS
 } from "../../../src/schema/preparation/ast-validation-modules/no-empty-object-types-validator";
 
 const modelWithObjectTypeWithFields = `
@@ -12,18 +12,33 @@ const modelWithObjectTypeWithFields = `
             }
         `;
 
-const modelWithObjectTypeWithoutFields = `
-            type Stuff {
+const modelWithRootEntityWithoutFields = `
+            type Stuff @rootEntity {
+            }
+        `;
+
+const modelWithValueObjectsWithoutFields = `
+            type Stuff @valueObject {
             }
         `;
 
 describe('no empty object types validator', () => {
-    it('finds object types without fields', () => {
-        const ast = parse(modelWithObjectTypeWithoutFields);
+    it('warns about rootEntities without fields', () => {
+        const ast = parse(modelWithRootEntityWithoutFields);
         const validationResult = new ValidationResult(new NoEmptyObjectTypesValidator().validate(ast));
         expect(validationResult.hasWarnings()).toBeTruthy();
+        expect(validationResult.hasErrors()).toBeFalsy();
         expect(validationResult.messages.length).toBe(1);
-        expect(validationResult.messages[0].msgKey).toBe(VALIDATION_WARNING_OBJECT_TYPE_WITHOUT_FIELDS);
+        expect(validationResult.messages[0].msgKey).toBe(VALIDATION_ERROR_OBJECT_TYPE_WITHOUT_FIELDS);
+    });
+
+    it('rejects valueObjects without fields', () => {
+        const ast = parse(modelWithValueObjectsWithoutFields);
+        const validationResult = new ValidationResult(new NoEmptyObjectTypesValidator().validate(ast));
+        expect(validationResult.hasWarnings()).toBeFalsy();
+        expect(validationResult.hasErrors()).toBeTruthy();
+        expect(validationResult.messages.length).toBe(1);
+        expect(validationResult.messages[0].msgKey).toBe(VALIDATION_ERROR_OBJECT_TYPE_WITHOUT_FIELDS);
     });
 
     it('accepts object types with fields', () => {
--- src/schema/preparation/ast-validation-modules/no-empty-object-types-validator.ts
+++ src/schema/preparation/ast-validation-modules/no-empty-object-types-validator.ts
@@ -1,9 +1,10 @@
 import {ASTValidator} from "../ast-validator";
 import {DocumentNode} from "graphql";
-import {ValidationMessage} from "../validation-message";
+import {Severity, ValidationMessage} from "../validation-message";
 import {getObjectTypes} from "../../schema-utils";
+import {CHILD_ENTITY_DIRECTIVE, OBJECT_TYPE_ENTITY_DIRECTIVES, ROOT_ENTITY_DIRECTIVE} from "../../schema-defaults";
 
-export const VALIDATION_WARNING_OBJECT_TYPE_WITHOUT_FIELDS = "Object type has no fields";
+export const VALIDATION_ERROR_OBJECT_TYPE_WITHOUT_FIELDS = "Object type has no fields";
 
 export class NoEmptyObjectTypesValidator implements ASTValidator {
 
@@ -11,7 +12,13 @@ export class NoEmptyObjectTypesValidator implements ASTValidator {
         const validationMessages: ValidationMessage[] = [];
             getObjectTypes(ast).forEach(objectType => {
                 if (!objectType.fields.length) {
-                    validationMessages.push(ValidationMessage.warn(VALIDATION_WARNING_OBJECT_TYPE_WITHOUT_FIELDS, {}, objectType.loc))
+                    const entityDirective = objectType.directives!
+                        .find(directive => OBJECT_TYPE_ENTITY_DIRECTIVES.includes(directive.name.value))!
+                        .name.value;
+                    const severity = [ROOT_ENTITY_DIRECTIVE, CHILD_ENTITY_DIRECTIVE].includes(entityDirective) ?
+                        Severity.Warning : Severity.Error;
+                    validationMessages.push(
+                        new ValidationMessage(severity, VALIDATION_ERROR_OBJECT_TYPE_WITHOUT_FIELDS, {}, objectType.loc))
                 }
             });
         return validationMessages;
