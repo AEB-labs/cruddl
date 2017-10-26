7d3a59e567ca44c8b9cdcc9cc99db1aa895af7ee
Sebastian Henke <s.henke@henke-informatik.de>
2017-10-26 19:17:15 +0200

    Improve validation and messages

--- src/schema/preparation/ast-validation-modules/every-root-entity-must-declare-one-role-validator.ts
+++ src/schema/preparation/ast-validation-modules/every-root-entity-must-declare-one-role-validator.ts
@@ -1,11 +1,12 @@
 import {ASTValidator} from "../ast-validator";
-import {DocumentNode} from "graphql";
+import {ArgumentNode, DocumentNode} from "graphql";
 import {ValidationMessage} from "../validation-message";
 import {findDirectiveWithName, getNodeByName, getRootEntityTypes} from "../../schema-utils";
 import {ROLES_DIRECTIVE, ROLES_READ_ARG, ROLES_READ_WRITE_ARG} from "../../schema-defaults";
-import {STRING} from "graphql/language/kinds";
+import {LIST, STRING} from "graphql/language/kinds";
 
-export const VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY = 'Every root entity must at least have one role which is allowed to access the entity.'
+export const VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY = 'Every root entity must at least have one role which is allowed to access the entity.';
+export const VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY_ILLEGAL_ARGUMENT_TYPE = 'Invalid argument type. Only String and list of Strings are allowed.';
 
 export class EveryRootEntityMustDeclareOneRoleValidator implements ASTValidator {
     validate(ast: DocumentNode): ValidationMessage[] {
@@ -17,8 +18,15 @@ export class EveryRootEntityMustDeclareOneRoleValidator implements ASTValidator 
                 } else {
                     const readArg = getNodeByName(rolesDirective.arguments, ROLES_READ_ARG);
                     const readWriteArg = getNodeByName(rolesDirective.arguments, ROLES_READ_WRITE_ARG);
-                    if ((!readArg || !readArg.value || readArg.value.kind !== STRING || !readArg.value.value) &&
-                        (!readWriteArg || !readWriteArg.value || readWriteArg.value.kind !== STRING || !readWriteArg.value.value)) {
+                    const readArgResult = checkArgPresent(readArg);
+                    if (readArgResult.validationMessage) {
+                        validationMessages.push(readArgResult.validationMessage);
+                    }
+                    const readWriteArgResult = checkArgPresent(readWriteArg);
+                    if (readWriteArgResult.validationMessage) {
+                        validationMessages.push(readWriteArgResult.validationMessage);
+                    }
+                    if (!readArgResult.result && !readWriteArgResult.result && !readArgResult.validationMessage && !readWriteArgResult.validationMessage) {
                         validationMessages.push(ValidationMessage.error(VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY, {}, rolesDirective.loc))
                     }
                 }
@@ -27,3 +35,26 @@ export class EveryRootEntityMustDeclareOneRoleValidator implements ASTValidator 
     }
 
 }
+
+function checkArgPresent(arg: ArgumentNode|undefined): {result: boolean, validationMessage?: ValidationMessage} {
+    if (!arg || !arg.value) {
+        return { result: false };
+    }
+    switch (arg.value.kind) {
+        case STRING:
+            if (!!arg.value.value) {
+                return { result: true };
+            } else {
+                return { result: false, validationMessage: ValidationMessage.error(VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY, {}, arg.value.loc) };
+            }
+        case LIST:
+            if (arg.value.values.some(val => !!val)) {
+                return { result: true };
+            } else {
+                return { result: false, validationMessage: ValidationMessage.error(VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY, {}, arg.value.loc) };
+            }
+        default:
+            return { result: false, validationMessage: ValidationMessage.error(VALIDATION_ERROR_MISSING_ROLE_ON_ROOT_ENTITY_ILLEGAL_ARGUMENT_TYPE, {}, arg.value.loc) };
+    }
+}
+
