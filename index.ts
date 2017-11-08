0304e53c454890a6329197e86e7f4c8d7172d920
Jan Melcher <jan.melcher@aeb.com>
2017-11-08 11:35:19 +0100

    Export SchemaConfig and SchemaPartConfig

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { createSchema } from './src/schema/schema-builder';
+export { SchemaConfig, SchemaPartConfig } from './src/config/schema-config';
 export { addQueryResolvers } from './src/query/query-resolvers';
 export { DatabaseAdapter } from './src/database/database-adapter';
 export * from './src/database/arangodb';
