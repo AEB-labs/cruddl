a2bbd2982821cee9e1584c7cc97e5e8348b494b6
Jan Melcher <jan.melcher@aeb.com>
2017-11-08 22:52:49 +0100

    Export directives and core scalar types

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export { ValidationResult,  } from './src/schema/preparation/ast-validator';
 export { SchemaConfig, SchemaPartConfig } from './src/config/schema-config';
 export { addQueryResolvers } from './src/query/query-resolvers';
 export { DatabaseAdapter } from './src/database/database-adapter';
+export { DIRECTIVES, CORE_SCALARS } from './src/schema/graphql-base';
 export * from './src/database/arangodb';
---
+++ src/schema/graphql-base.ts
@@ -0,0 +1,42 @@
+import gql from 'graphql-tag';
+
+export const DIRECTIVES = gql`
+    # Declares a type for root-level objects with ids that are stored directly in the data base
+    directive @rootEntity on OBJECT
+    
+    # Declares a type for objects with ids that can be embedded as a list within another entity
+    directive @childEntity on OBJECT
+    
+    # Declares a type for objects without id that can be embedded everywhere and can only be replaced as a whole
+    directive @valueObject on OBJECT
+    
+    # Declares a type for objects which can be embedded within entities or entity extensions
+    directive @entityExtension on OBJECT
+    
+    # Declares a field as a to-1 or to-n relation to another root entity
+    directive @relation on FIELD_DEFINITION
+    
+    # Declares a field to reference another root entity via its @key
+    directive @reference on FIELD_DEFINITION
+    
+    # Declares a field as business key which is used in @reference fields
+    directive @key on FIELD_DEFINITION
+    
+    # Specifies the namespace of a type
+    directive @namespace(name: String!) on OBJECT
+    
+    # Specifies the roles that can access objects of this type
+    directive @roles(
+        # A list of roles that are authorized to read objects of this type
+        read: [String!]
+        # A list of roles that are authorized to read, create, update and delete objects of this type
+        readWrite: [String!])
+    on FIELD_DEFINITION|OBJECT
+`;
+
+export const CORE_SCALARS = gql`
+    scalar DateTime
+    scalar Date
+    scalar Time
+    scalar JSON
+`;
