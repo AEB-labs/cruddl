95f073a93703b7f9de6850f5f19a0b003af28961
Sebastian Henke <s.henke@henke-informatik.de>
2017-11-14 14:22:37 +0100

    Reduce colors in output of JSON diff
    Use JasmineConsoleReporter to have better colors and reduce JSON comparison size. Don't show ACTUAL and EXPECTED anymore because it's now readable in the diff.

--- momo-test.js
+++ momo-test.js
@@ -1,8 +1,11 @@
 // Same as just running jasmine. Is used to be able to pass arguments to node
+const JasmineConsoleReporter = require('jasmine-console-reporter');
 
 var Jasmine = require('jasmine');
 var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 var jasmine = new Jasmine();
 jasmine.loadConfigFile('./spec/support/jasmine.json');
-jasmine.addReporter(new SpecReporter());
+jasmine.addReporter(new JasmineConsoleReporter({
+    colors: 1
+}));
 jasmine.execute();
