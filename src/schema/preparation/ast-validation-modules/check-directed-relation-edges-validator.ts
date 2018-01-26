5b3074091005c6a5e7c968c83f9fc4dc3f11157a
Jan Melcher <jan.melcher@aeb.com>
2018-01-26 23:35:07 +0100

    Improve validation messages for relations

--- src/schema/preparation/ast-validation-modules/check-directed-relation-edges-validator.ts
+++ src/schema/preparation/ast-validation-modules/check-directed-relation-edges-validator.ts
@@ -11,15 +11,15 @@ import {INVERSE_OF_ARG, RELATION_DIRECTIVE} from "../../schema-defaults";
 import {STRING} from "graphql/language/kinds";
 
 export const VALIDATION_ERROR_INVALID_ARGUMENT_TYPE = 'Invalid argument type.';
-export const VALIDATION_ERROR_INVERSE_FIELD_NOT_FOUND = `Field specified in ${INVERSE_OF_ARG} argument not found on the other side`;
-export const VALIDATION_WARNING_UNASSOCIATED_RELATIONS = 'In this type, there is a relation to the other type and in the other type, there is a relation to this type. None of them is declared as inverseOf the other one. This is possibly a miss-configuration and leads to independent relations. ATTENTION: When editing an existing relation, always add the inverseOf argument to the @relation which was created last.';
+
 class EdgeSide {
     constructor(
         readonly localType: ObjectTypeDefinitionNode,
         readonly localField: FieldDefinitionNode,
         readonly remoteType: string,
         readonly inverseOf?: string,
-        readonly loc?: Location) {}
+        readonly loc?: Location,
+        readonly inverseOfArgValueLoc?: Location) {}
 }
 
 export class CheckDirectedRelationEdgesValidator implements ASTValidator {
@@ -46,7 +46,7 @@ export class CheckDirectedRelationEdgesValidator implements ASTValidator {
                             actual: inverseOfArg.value.kind
                         }, inverseOfArg.value.loc))
                     } else {
-                        endingEdges.push(new EdgeSide(rootEntity, field, getTypeNameIgnoringNonNullAndList(field.type), inverseOfArg.value.value, relationDirective.loc))
+                        endingEdges.push(new EdgeSide(rootEntity, field, getTypeNameIgnoringNonNullAndList(field.type), inverseOfArg.value.value, relationDirective.loc, inverseOfArg.value.loc))
                     }
                 } else {
                     startingEdges.push(new EdgeSide(rootEntity, field, getTypeNameIgnoringNonNullAndList(field.type), undefined, relationDirective.loc))
@@ -69,7 +69,7 @@ function matchStartingAndEndingEdges(startingEdges: EdgeSide[], endingEdges: Edg
             endingEdges = endingEdges.filter(ee => ee !== endingEdge);
         } else {
             const messageVars = { localType: endingEdge.localType.name.value, localField: endingEdge.localField.name.value, inverseOf: endingEdge.inverseOf || '' };
-            validationMessages.push(ValidationMessage.error(VALIDATION_ERROR_INVERSE_FIELD_NOT_FOUND, messageVars, endingEdge.loc));
+            validationMessages.push(ValidationMessage.error(`Type "${endingEdge.remoteType}" does not have a field "${endingEdge.inverseOf}"`, messageVars, endingEdge.inverseOfArgValueLoc));
         }
     });
     // Now, check if there are starting edges which could be miss-configured (due to missing inverseOf)
@@ -77,7 +77,8 @@ function matchStartingAndEndingEdges(startingEdges: EdgeSide[], endingEdges: Edg
         startingEdges.forEach(edge2 => {
             if (edge1.remoteType === edge2.localType.name.value
                 && edge2.remoteType === edge1.localType.name.value) {
-                validationMessages.push(ValidationMessage.warn(VALIDATION_WARNING_UNASSOCIATED_RELATIONS, { localType: edge1.localType.name.value, localField: edge1.localField.name.value }, edge1.loc));
+                validationMessages.push(ValidationMessage.warn(`This field and "${edge1.remoteType}.${edge2.localField.name.value}" define separate relations. Consider using the "inverseOf" argument to add a backlink to an existing relation`,
+                    { localType: edge1.localType.name.value, localField: edge1.localField.name.value }, edge1.loc));
             }
         })
     })
