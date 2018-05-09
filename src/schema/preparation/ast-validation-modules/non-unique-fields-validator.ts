8228957673668e3cf0a0a05f6103e35ebcc43377
Nils Wenzler <nils.wenzler@aeb.com>
2018-05-09 10:44:03 +0200

    Introduce new check for non-unique fields in an entity

---
+++ src/schema/preparation/ast-validation-modules/non-unique-fields-validator.ts
@@ -0,0 +1,21 @@
+import {ASTValidator} from "../ast-validator";
+import {DocumentNode} from "graphql";
+import {ValidationMessage} from "../validation-message";
+import {getObjectTypes} from "../../schema-utils";
+
+export const VALIDATION_ERROR_NON_UNIQUE_FIELDS = "A field with the same name already exists in this type.";
+
+export class NonUniqueFieldsValidator implements ASTValidator {
+
+    validate(ast: DocumentNode): ValidationMessage[] {
+        const validationMessages: ValidationMessage[] = [];
+        getObjectTypes(ast).forEach(objectTypeDefinition => {
+            objectTypeDefinition.fields.forEach(field => {
+                if(objectTypeDefinition.fields.find((comparisonField) => comparisonField !== field && comparisonField.name.value === field.name.value)) {
+                    validationMessages.push(ValidationMessage.error(VALIDATION_ERROR_NON_UNIQUE_FIELDS, {type: objectTypeDefinition.name.value}, field.loc));
+                }
+            });
+        });
+        return validationMessages;
+    }
+}
