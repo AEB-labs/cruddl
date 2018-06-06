56408e5d01f49b8af480e408731dbab59e3c7544
Jan Melcher <jan.melcher@aeb.com>
2018-06-06 14:58:52 +0200

    Increase timeout for regression tests

--- spec/regression/regressions.spec.ts
+++ spec/regression/regressions.spec.ts
@@ -32,7 +32,7 @@ describe('regression tests', async () => {
                         it(testName, async () => {
                             const {expectedResult, actualResult} = await suite.runTest(testName);
                             expect(actualResult).to.deep.equal(expectedResult);
-                        });
+                        }).timeout(10000); // travis is sometimes on the slower side
                     }
                 });
             }
