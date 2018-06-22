1d8fbef373415645f7673511464b3266a0893a24
Nils Wenzler <nils.wenzler@aeb.com>
2018-06-22 11:22:54 +0200

    Always add empty filter.

--- src/schema-generation/utils/filtering.ts
+++ src/schema-generation/utils/filtering.ts
@@ -5,10 +5,7 @@ import { decapitalize } from '../../utils/utils';
 import { FilterObjectType } from '../filter-input-types';
 
 export function buildFilteredListNode(listNode: QueryNode, args: {[name: string]: any}, filterType: FilterObjectType, itemType: Type) {
-    const filterValue = args[FILTER_ARG];
-    if (filterValue == undefined) {
-        return listNode;
-    }
+    const filterValue = args[FILTER_ARG] || {};
 
     const itemVariable = new VariableQueryNode(decapitalize(itemType.name));
     const filterNode = filterValue != undefined ? filterType.getFilterNode(itemVariable, args[FILTER_ARG]) : undefined;
