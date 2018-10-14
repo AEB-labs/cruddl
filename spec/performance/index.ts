1ef476a829f07479b7cf5e0ac407afccc31e5204
Jan Melcher <jan.melcher@aeb.com>
2018-10-14 21:53:50 +0200

    Add performance tests for string filters

--- spec/performance/index.ts
+++ spec/performance/index.ts
@@ -4,13 +4,15 @@ import {default as PAGINATION} from "./pagination.perf";
 import {default as COUNT} from "./count.perf";
 import {default as ASSOCIATIONS} from "./associations.perf";
 import {default as QUERY_PIPELINE} from "./query-pipeline.perf";
+import {default as FILTER} from "./filter.perf";
 import {default as REFERENCES} from "./references.perf";
 
 runBenchmarks([
-    //...CRUD,
+    ...CRUD,
     ...PAGINATION,
-    //...COUNT,
-    //...REFERENCES,
-    //...ASSOCIATIONS,
-    //...QUERY_PIPELINE
+    ...COUNT,
+    ...REFERENCES,
+    ...ASSOCIATIONS,
+    ...QUERY_PIPELINE,
+    ...FILTER
 ]);
