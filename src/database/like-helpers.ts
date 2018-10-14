11263f307a5cb23519e50f4f07b697fdf00422ed
Jan Melcher <jan.melcher@aeb.com>
2018-10-14 21:54:41 +0200

    Add _like filter operator and use skiplist indices where possible
    We can not just add skiplist support to _starts_with because it is case-sensitive (as is equals)
    
    The _like filter uses a skiplist for literal searches (will be
    case-insensitive then) and otherwise for the first literal prefix. If
    the whole filter is a prefix, it can be fully covered by the index
    and no in-memory LIKE is needed.
    
    See this issue: https://github.com/arangodb/arangodb/issues/6877

---
+++ src/database/like-helpers.ts
@@ -0,0 +1,79 @@
+import { escapeRegExp } from '../utils/utils';
+
+/**
+ * Gets the literal part until the first placeholder. Also returns whether the literal part is followed by a simple %
+ * placeholder, or if it even only consists of the literal part
+ * @param pattern a LIKE pattern with % and _ as placeholders (escaped with \)
+ */
+export function analyzeLikePatternPrefix(pattern: string): { literalPrefix: string, isSimplePrefixPattern: boolean, isLiteralPattern: boolean } {
+    // pattern: % and _ are placeholders, can be escaped with backslash
+    let literalPrefix = '';
+    let i = 0;
+    while (i < pattern.length) {
+        const char = pattern[i];
+        switch (char) {
+            case '\\':
+                const nextChar = pattern[i + 1];
+                if (nextChar === '%' || nextChar === '_' || nextChar === '\\') {
+                    literalPrefix += nextChar;
+                    i += 2;
+                } else {
+                    // might be trailing backslash or an unescaped backslash, (e.g. \\a will be interpreted like
+                    // \\\\a) - to stay consistent with ArangoDB
+                    literalPrefix += char;
+                    i++;
+                }
+                break;
+            case '%':
+                // normally, for simple prefixes ending in %, the remainder would be empty - but in case there are
+                // multiple % placeholders, this is equivalent.
+                const remainder = pattern.substr(i + 1);
+                const isSimplePrefixPattern = Array.from(remainder).every(c => c === '%');
+                return { literalPrefix, isSimplePrefixPattern, isLiteralPattern: false };
+            case '_':
+                return { literalPrefix, isSimplePrefixPattern: false, isLiteralPattern: false };
+            default:
+                literalPrefix += pattern[i];
+                i++;
+                break;
+        }
+    }
+    // we only encountered literal characters (all others would cause an early return in the switch statement)
+    return { literalPrefix, isSimplePrefixPattern: false, isLiteralPattern: true };
+}
+
+export function likePatternToRegExp(pattern: string): RegExp {
+    let regex = '';
+    let i = 0;
+    while (i < pattern.length) {
+        const char = pattern[i];
+        switch (char) {
+            case '\\':
+                const nextChar = pattern[i + 1];
+                if (nextChar === '%' || nextChar === '_' || pattern[ + 1] === '\\') {
+                    regex += escapeRegExp(nextChar);
+                    i += 2;
+                } else {
+                    // might be trailing backslash or an unescaped backslash, (e.g. \\a will be interpreted like
+                    // \\\\a) - to stay consistent with ArangoDB
+                    regex += escapeRegExp(char);
+                    i++;
+                }
+                break;
+            case '%':
+                // no dotall modifier for now in JavaScript (there is a proposal for it though)
+                regex += '([\\s\\S]*)';
+                i++;
+                break;
+            case '_':
+                regex += '[\\s\\S]';
+                i++;
+                break;
+            default:
+                regex += escapeRegExp(char);
+                i++;
+                break;
+        }
+    }
+    return RegExp('^' + regex + '$', 'i'); // i: case insensitive
+}
