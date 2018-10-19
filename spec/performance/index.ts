fd01f132e476a07e041ae9310b088d844b5d5850
Jan Melcher <jan.melcher@aeb.com>
2018-10-19 18:25:23 +0200

    Add performance test for @reference

--- spec/performance/index.ts
+++ spec/performance/index.ts
@@ -4,11 +4,13 @@ import {default as PAGINATION} from "./pagination.perf";
 import {default as COUNT} from "./count.perf";
 import {default as ASSOCIATIONS} from "./associations.perf";
 import {default as QUERY_PIPELINE} from "./query-pipeline.perf";
+import {default as REFERENCES} from "./references.perf";
 
 runBenchmarks([
     ...CRUD,
     ...PAGINATION,
     ...COUNT,
+    ...REFERENCES,
     ...ASSOCIATIONS,
     ...QUERY_PIPELINE
 ]);
