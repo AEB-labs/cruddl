fbde00df62287d3f8d25bc155c212bbe0296fdd1
Jan Melcher <jan.melcher@aeb.com>
2019-02-06 19:08:44 +0100

    Pre-compile json schema to support a CSP with eval disallowed

--- src/schema/preparation/source-validation-modules/sidecar-schema.ts
+++ src/schema/preparation/source-validation-modules/sidecar-schema.ts
@@ -1,19 +1,18 @@
 import { ParsedProjectSource, ParsedProjectSourceBaseKind } from '../../../config/parsed-project';
-import { ParsedSourceValidator } from '../ast-validator';
 import { ValidationMessage } from '../../../model';
-import ajv = require('ajv');
+import { ParsedSourceValidator } from '../ast-validator';
+import * as validate from './schema/validate-schema';
 
 export class SidecarSchemaValidator implements ParsedSourceValidator {
     validate(source: ParsedProjectSource): ValidationMessage[] {
-        if (source.kind != ParsedProjectSourceBaseKind.OBJECT ) {
+        if (source.kind != ParsedProjectSourceBaseKind.OBJECT) {
             return [];
         }
 
         let data = source.object;
 
-        const validate = ajv({
-            allErrors: true
-        }).compile(schemaJSON);
+        // validate-schema.js is generated from schema.json using the npm script "compile-json-schema"
+        // we pre-compile this not only for performance but mainly to conform to a CSP with eval disallowed
         if (validate(data) || !validate.errors) {
             return [];
         }
@@ -22,7 +21,7 @@ export class SidecarSchemaValidator implements ParsedSourceValidator {
             const path = reformatPath(err.dataPath);
             if (path in source.pathLocationMap) {
                 const loc = source.pathLocationMap[path];
-                return ValidationMessage.error(err.message!,  loc);
+                return ValidationMessage.error(err.message!, loc);
             } else {
                 return ValidationMessage.error(`${err.message} (at ${err.dataPath})`, undefined);
             }
@@ -37,170 +36,3 @@ function reformatPath(path: string) {
         .replace(/\[\'([^']*)\'\]/g, (_, name) => `/${name}`)
         .replace(/\[([^\]])*\]/g, (_, name) => `/${name}`);
 }
-
-// TODO: JSON Schema should be extracted into separate file
-const schemaJSON = JSON.parse(`{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "description": "Sidecar file for schema definitions",
-  "type": "object",
-  "minProperties": 1,
-  "additionalProperties": false,
-  "properties": {
-    "permissionProfiles": {
-      "type": "object",
-      "additionalProperties": false,
-      "patternProperties": {
-        "^[a-zA-Z0-9]+$": {
-          "$ref": "#/definitions/PermissionProfile"
-        }
-      }
-    },
-    "i18n": {
-      "type": "object",
-      "additionalProperties": false,
-      "patternProperties": {
-        "^[a-zA-Z0-9_-]+$": {
-          "$ref": "#/definitions/NamespaceLocalization"
-        }
-      }
-    }
-  },
-  "definitions": {
-    "PermissionProfile": {
-      "type": "object",
-      "required": [
-        "permissions"
-      ],
-      "additionalProperties": false,
-      "properties": {
-        "permissions": {
-          "type": "array",
-          "minLength": 1,
-          "items": {
-            "$ref": "#/definitions/Permission"
-          }
-        }
-      }
-    },
-    "Permission": {
-      "type": "object",
-      "required": [
-        "roles",
-        "access"
-      ],
-      "additionalProperties": false,
-      "properties": {
-        "roles": {
-          "type": "array",
-          "minLength": 1,
-          "items": {
-            "type": "string",
-            "pattern": ".+"
-          }
-        },
-        "access": {
-          "type": "string",
-          "enum": [
-            "read",
-            "readWrite"
-          ]
-        },
-        "restrictToAccessGroups": {
-          "type": "array",
-          "minLength": 1,
-          "items": {
-            "type": "string",
-            "pattern": ".+"
-          }
-        }
-      }
-    },
-    "NamespaceLocalization": {
-      "type": "object",
-      "additionalProperties": false,
-      "properties": {
-        "types": {
-          "patternProperties": {
-            "^[a-zA-Z0-9_]+$": {
-              "$ref": "#/definitions/TypeLocalization"
-            }
-          }
-        },
-        "fields": {
-          "patternProperties": {
-            "^[a-zA-Z0-9_]+$": {
-              "anyOf": [
-                { "$ref": "#/definitions/FieldLocalization" },
-                { "type": "string" }
-              ]
-            }
-          }
-        }
-      }
-    },
-    "TypeLocalization": {
-      "type": "object",
-      "additionalProperties": false,
-      "properties": {
-        "fields": {
-          "patternProperties": {
-            "^[a-zA-Z0-9_]+$": {
-              "anyOf": [
-                {
-                  "$ref": "#/definitions/FieldLocalization"
-                },
-                {
-                  "type": "string"
-                }
-              ]
-            }
-          }
-        },
-        "values": {
-          "patternProperties": {
-            "^[a-zA-Z0-9_]+$": {
-              "anyOf": [
-                {
-                  "$ref": "#/definitions/EnumValueLocalization"
-                },
-                {
-                  "type": "string"
-                }
-              ]
-            }
-          }
-        },
-        "label": {
-          "type": "string"
-        },
-        "labelPlural": {
-          "type": "string"
-        },
-        "hint": {
-          "type": "string"
-        }
-      }
-    },
-    "FieldLocalization": {
-      "properties": {
-        "label": {
-          "type": "string"
-        },
-        "hint": {
-          "type": "string"
-        }
-      }
-    },
-    "EnumValueLocalization": {
-      "properties": {
-        "label": {
-          "type": "string"
-        },
-        "hint": {
-          "type": "string"
-        }
-      }
-    }
-  }
-}
-`);
