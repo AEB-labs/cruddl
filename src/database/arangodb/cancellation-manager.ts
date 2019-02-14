ff8e360df9d958631ac3a63c87dfdbfa82c7dd94
Jan Melcher <jan.melcher@aeb.com>
2019-02-14 09:42:01 +0100

    Kill ArangoDB queries instead of just cancelling the request
    ArangoDB does not terminate queries when the request is aborted, so we
    need to find the queries and kill them.

---
+++ src/database/arangodb/cancellation-manager.ts
@@ -0,0 +1,31 @@
+import { Database } from 'arangojs';
+import { ERROR_QUERY_NOT_FOUND } from './error-codes';
+
+interface CancellationManagerConfig {
+    readonly database: Database
+}
+
+export class CancellationManager {
+    private readonly database: Database;
+
+    constructor(config: CancellationManagerConfig) {
+        this.database = config.database;
+    }
+
+    async cancelQuery(transactionID: string) {
+        const queries = await this.database.listRunningQueries();
+        const query = queries.find(q => q.query.startsWith(`/*id:${transactionID}*/`));
+        if (!query) {
+            return;
+        }
+        try {
+            await this.database.killQuery(query.id);
+        } catch (e) {
+            if (e.errorNum === ERROR_QUERY_NOT_FOUND) {
+                // error has finished in the meantime, so ignore
+                return;
+            }
+            throw e;
+        }
+    }
+}
