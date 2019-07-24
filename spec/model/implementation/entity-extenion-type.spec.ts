a30e4a55d1c247522b8a43389cface239df7779d
msf <msf@aeb.com>
2019-07-24 14:15:25 +0200

    Add validation to prevent recursive entity extension definitions

---
+++ spec/model/implementation/entity-extenion-type.spec.ts
@@ -0,0 +1,74 @@
+import { TypeKind } from '../../../src/model/config';
+import { EntityExtensionType, Model, RootEntityType } from '../../../src/model/implementation';
+import { Severity } from '../../../src/model/validation';
+import { expectSingleMessageToInclude } from './validation-utils';
+
+describe('EnityExtensionType', () => {
+    it('rejects EntityExtensions with recursion', () => {
+        const type = new EntityExtensionType({
+            kind: TypeKind.ENTITY_EXTENSION,
+            name: 'DeliveryInfo',
+            fields: [
+                {
+                    name: 'deliveryInfo',
+                    typeName: 'DeliveryInfo'
+                }
+            ]
+        }, new Model({
+            types: [
+                {
+                    kind: TypeKind.ENTITY_EXTENSION,
+                    name: 'DeliveryInfo',
+                    fields: [
+                        {
+                            name: 'deliveryInfo',
+                            typeName: 'DeliveryInfo'
+                        }
+                    ]
+                }
+            ]
+        }));
+
+
+        expectSingleMessageToInclude(type, `EntityTypes cannot recursively contain an EntityType of their own type.`, Severity.Error);
+    });
+
+    it('rejects EntityExtensions with nested recursion', () => {
+        const type = new EntityExtensionType({
+            kind: TypeKind.ENTITY_EXTENSION,
+            name: 'DeliveryInfo',
+            fields: [
+                {
+                    name: 'otherInfo',
+                    typeName: 'OtherInfo'
+                }
+            ]
+        }, new Model({
+            types: [
+                {
+                    kind: TypeKind.ENTITY_EXTENSION,
+                    name: 'DeliveryInfo',
+                    fields: [
+                        {
+                            name: 'deliveryInfo',
+                            typeName: 'DeliveryInfo'
+                        }
+                    ]
+                },
+                {
+                    kind: TypeKind.ENTITY_EXTENSION,
+                    name: 'OtherInfo',
+                    fields: [
+                        {
+                            name: 'deliveryInfo',
+                            typeName: 'DeliveryInfo'
+                        }
+                    ]
+                }
+            ]
+        }));
+
+
+        expectSingleMessageToInclude(type, `EntityTypes cannot recursively contain an EntityType of their own type.`, Severity.Error);
+    });
+});
--- src/model/implementation/entity-extension-type.ts
+++ src/model/implementation/entity-extension-type.ts
@@ -1,3 +1,6 @@
+import { flatMap } from '../../utils/utils';
+import { ValidationContext, ValidationMessage } from '../validation';
+import { Field } from './field';
 import { ObjectTypeBase } from './object-type-base';
 import { EntityExtensionTypeConfig, TypeKind } from '../config';
 import { Model } from './model';
@@ -12,4 +15,33 @@ export class EntityExtensionType extends ObjectTypeBase {
     readonly isRootEntityType: false = false;
     readonly isEntityExtensionType: true = true;
     readonly isValueObjectType: false = false;
+
+
+    validate(context: ValidationContext) {
+        super.validate(context);
+
+        this.validateRecursion(context);
+    }
+
+    private validateRecursion(context: ValidationContext) {
+        const thisType = this;
+
+        function fieldContainsRecursion(field: Field): boolean {
+            if (field.type.name === thisType.name && field.type.namespace === thisType.namespace) {
+                return true;
+            } else if (field.type.isEntityExtensionType) {
+                return field.type.fields.some(value => fieldContainsRecursion(value));
+            } else {
+                return false;
+            }
+        }
+
+        const recursiveFields = this.fields.filter(fieldContainsRecursion);
+
+        recursiveFields.forEach(value => {
+            context.addMessage(ValidationMessage.error(`EntityTypes cannot recursively contain an EntityType of their own type.`, value.astNode));
+        });
+
+    }
+
 }
