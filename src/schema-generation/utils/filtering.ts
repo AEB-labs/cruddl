c2d4025cc6bc7c2205ba0a1b0b70053eb1b8d26a
Jan Melcher <jan.melcher@aeb.com>
2020-02-18 13:38:24 +0100

    Add create/update/delete many mutations
    # Conflicts:
    #	spec/regression/logistics/tests/descriptions.result.json
    #	src/schema-generation/query-node-object-type/convert.ts

--- src/schema-generation/utils/filtering.ts
+++ src/schema-generation/utils/filtering.ts
@@ -1,11 +1,23 @@
 import { Type } from '../../model';
-import { ConstBoolQueryNode, QueryNode, TransformListQueryNode, VariableQueryNode } from '../../query-tree';
+import {
+    BinaryOperationQueryNode,
+    BinaryOperator,
+    ConstBoolQueryNode,
+    QueryNode,
+    TransformListQueryNode,
+    VariableQueryNode
+} from '../../query-tree';
 import { simplifyBooleans } from '../../query-tree/utils';
 import { FILTER_ARG } from '../../schema/constants';
 import { decapitalize } from '../../utils/utils';
 import { FilterObjectType } from '../filter-input-types';
 
-export function buildFilteredListNode(listNode: QueryNode, args: { [name: string]: any }, filterType: FilterObjectType, itemType: Type) {
+export function buildFilteredListNode(
+    listNode: QueryNode,
+    args: { [name: string]: any },
+    filterType: FilterObjectType,
+    itemType: Type
+) {
     const filterValue = args[FILTER_ARG] || {};
     const itemVariable = new VariableQueryNode(decapitalize(itemType.name));
     // simplification is important for the shortcut with check for TRUE below in the case of e.g. { AND: [] }
@@ -16,6 +28,27 @@ export function buildFilteredListNode(listNode: QueryNode, args: { [name: string
         return listNode;
     }
 
+    return new TransformListQueryNode({
+        listNode,
+        itemVariable,
+        filterNode
+    });
+}
+
+export function getFilterNode(listNode: QueryNode, predicate: (itemNode: QueryNode) => QueryNode) {
+    if (listNode instanceof TransformListQueryNode) {
+        return new TransformListQueryNode({
+            ...listNode,
+            filterNode: new BinaryOperationQueryNode(
+                listNode.filterNode,
+                BinaryOperator.AND,
+                predicate(listNode.itemVariable)
+            )
+        });
+    }
+
+    const itemVariable = new VariableQueryNode('item');
+    const filterNode = predicate(itemVariable);
     return new TransformListQueryNode({
         listNode,
         itemVariable,
