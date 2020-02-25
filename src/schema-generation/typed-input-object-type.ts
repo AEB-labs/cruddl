09c7c92979d1a35b6032a5a255067af3cc5b17b8
Jan Melcher <jan.melcher@aeb.com>
2020-02-25 11:08:31 +0100

    Include deprecation reason of input fields in description

--- src/schema-generation/typed-input-object-type.ts
+++ src/schema-generation/typed-input-object-type.ts
@@ -5,10 +5,10 @@ import { Constructor } from '../utils/utils';
 import { resolveThunk } from './query-node-object-type';
 
 export interface TypedInputFieldBase<TField extends TypedInputFieldBase<TField>> {
-    readonly name: string
-    readonly description?: string
-    readonly deprecationReason?: string
-    readonly inputType: GraphQLInputType | TypedInputObjectType<TField>
+    readonly name: string;
+    readonly description?: string;
+    readonly deprecationReason?: string;
+    readonly inputType: GraphQLInputType | TypedInputObjectType<TField>;
 }
 
 export class TypedInputObjectType<TField extends TypedInputFieldBase<TField>> {
@@ -17,27 +17,43 @@ export class TypedInputObjectType<TField extends TypedInputFieldBase<TField>> {
         private readonly _fields: Thunk<ReadonlyArray<TField>>,
         public readonly description?: string,
         public readonly deprecationReason?: string
-    ) {
-    }
+    ) {}
 
     @memorize()
     getInputType(): GraphQLInputObjectType {
         let description = this.description;
         if (this.deprecationReason) {
-            // Input fields can not yet be deprecated (see https://github.com/graphql/graphql-spec/pull/525)
+            // Input types can not be deprecated officially
             description = description ? `${description}\n\n${this.deprecationReason}` : this.deprecationReason;
         }
 
         return new GraphQLInputObjectType({
             name: this.name,
             description,
-            fields: () => chain(this.fields)
-                .keyBy(field => field.name)
-                .mapValues((field): GraphQLInputFieldConfig => ({
-                    type: field.inputType instanceof TypedInputObjectType ? field.inputType.getInputType() : field.inputType,
-                    description: field.description
-                }))
-                .value()
+            fields: () =>
+                chain(this.fields)
+                    .keyBy(field => field.name)
+                    .mapValues(
+                        (field): GraphQLInputFieldConfig => {
+                            let description = field.description;
+
+                            if (field.deprecationReason) {
+                                // Input fields can not yet be deprecated (see https://github.com/graphql/graphql-spec/pull/525)
+                                description = description
+                                    ? `${description}\n\n${field.deprecationReason}`
+                                    : field.deprecationReason;
+                            }
+
+                            return {
+                                type:
+                                    field.inputType instanceof TypedInputObjectType
+                                        ? field.inputType.getInputType()
+                                        : field.inputType,
+                                description
+                            };
+                        }
+                    )
+                    .value()
         });
     }
 
@@ -47,14 +63,16 @@ export class TypedInputObjectType<TField extends TypedInputFieldBase<TField>> {
             throw new Error(`Expected field "${name}" to exist on input object type "${this.name}"`);
         }
         if (clazz && !(field instanceof clazz)) {
-            throw new Error(`Expected input field "${this.name}.${name}" to be of type "${clazz.name}", but is of type "${field.constructor.name}"`);
+            throw new Error(
+                `Expected input field "${this.name}.${name}" to be of type "${clazz.name}", but is of type "${field.constructor.name}"`
+            );
         }
         return field as T;
     }
 
     @memorize()
     private get fieldMap() {
-        return new Map(this.fields.map((field): [string, TField] => ([field.name, field])));
+        return new Map(this.fields.map((field): [string, TField] => [field.name, field]));
     }
 
     @memorize()
@@ -63,10 +81,15 @@ export class TypedInputObjectType<TField extends TypedInputFieldBase<TField>> {
     }
 }
 
-function resolveAndCheckFields<TField extends TypedInputFieldBase<TField>>(thunk: Thunk<ReadonlyArray<TField>>, typeName: string): ReadonlyArray<TField> {
+function resolveAndCheckFields<TField extends TypedInputFieldBase<TField>>(
+    thunk: Thunk<ReadonlyArray<TField>>,
+    typeName: string
+): ReadonlyArray<TField> {
     const fields = resolveThunk(thunk);
     if (uniqBy(fields, field => field.name).length !== fields.length) {
-        throw new Error(`Input type "${typeName}" has duplicate fields (fields: ${fields.map(f => f.name).join(', ')})`);
+        throw new Error(
+            `Input type "${typeName}" has duplicate fields (fields: ${fields.map(f => f.name).join(', ')})`
+        );
     }
     return fields;
 }
