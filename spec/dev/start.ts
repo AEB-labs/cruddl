2092bb1243e219ba73c9ba2c4e0dbbfda737997a
Jan Melcher <jan.melcher@aeb.com>
2022-09-06 14:57:35 +0200

    Remove line in test server that disabled TLS verification
    This was only present in the test server run locally in this repo.
    Nevertheless, it is not a good idea to have something like this
    anywhere in the code.

--- spec/dev/start.ts
+++ spec/dev/start.ts
@@ -1,8 +1,5 @@
 import { start } from './server';
 
-// to get through firewall
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 import colors from '../../src/utils/colors';
 colors.enabled = true;
 
