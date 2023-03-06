57c17124a783f4a1f0585a8f0c1d07a4b17d583e
Jan Melcher <jan.melcher@aeb.com>
2023-03-06 20:04:37 +0100

    build: fix build error because package version could not be resolved
    The built package has a different directory structure (./dist/src) than
    the one used for local development (./src), so the package.json cannot
    be resolved with a relative path.
    
    A simple file replacement is probably less effort than a fancy file path
    resolution which might break with webpack etc.
    
    An alternative might be to have a directory structure where ./dist and
    ./src live side-by-side. This could be a possible change for the future.

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 rimraf dist
 tsc --skipLibCheck
 cpy --flat src/schema/preparation/source-validation-modules/schema dist/src/schema/preparation/source-validation-modules/schema
+node ./emit-cruddl-version.js
 dependency-check ./package.json --no-dev --ignore-module @arangodb --ignore-module internal
