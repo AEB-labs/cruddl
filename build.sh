1499f56ca8e5fcfd832d5a678e1e05e5fc7abbd1
Jan Melcher <jan.melcher@aeb.com>
2024-07-25 11:06:37 +0200

    build: replace deprecated "dependency-check" with knip
    depdendency-check started to fail with an error that does not even include
    a stack trace.
    
    knip does a little bit more than dependency-check did. We disable some of
    the rules because they just generate noise for now.
    
    A note about the knip.json: every file suffixed by "!" is one that is used
    in prod mode (npm run knip:prod).

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ rimraf dist
 tsc --skipLibCheck
 cpy --flat src/schema/preparation/source-validation-modules/schema dist/src/schema/preparation/source-validation-modules/schema
 node ./emit-cruddl-version.js
-dependency-check ./package.json --no-dev --ignore-module @arangodb --ignore-module internal
+# check e.g. for imports from modules that are not listed as dependencies
+npm run knip
+npm run knip:prod
