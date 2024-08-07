0974a21bcf76d43c12a43dcb0c20fda11a15da4a
Jan Melcher <jan.melcher@aeb.com>
2024-08-07 18:18:30 +0200

    feat: add quick fix for "field is missing" compatibility issue

--- spec/model/compatibility-check/check-object-types.spec.ts
+++ spec/model/compatibility-check/check-object-types.spec.ts
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { expectSingleCompatibilityIssue } from '../implementation/validation-utils';
+import { expectQuickFix, expectSingleCompatibilityIssue } from '../implementation/validation-utils';
 import { runCheck } from './utils';
 
 describe('checkModel', () => {
@@ -20,6 +20,45 @@ describe('checkModel', () => {
             expectSingleCompatibilityIssue(
                 result,
                 'Field "Test.field" is missing (required by module "module1").',
+            );
+            expectQuickFix(
+                result,
+                'Add field "field"',
+                `type Test @rootEntity {
+  wrongFieldName: String
+  field: String
+}`,
+            );
+        });
+
+        it('includes field directives in the quickfix', () => {
+            const result = runCheck(
+                gql`
+                    type Test @rootEntity @modules(in: "module1") {
+                        field: String
+                            @calcMutations(operators: APPEND)
+                            @key
+                            @roles(read: ["a", "b"])
+                            @modules(all: true)
+                    }
+                `,
+                gql`
+                    type Test @rootEntity {
+                        wrongFieldName: String
+                    }
+                `,
+            );
+            expectSingleCompatibilityIssue(
+                result,
+                'Field "Test.field" is missing (required by module "module1").',
+            );
+            expectQuickFix(
+                result,
+                'Add field "field"',
+                `type Test @rootEntity {
+  wrongFieldName: String
+  field: String @calcMutations(operators: APPEND) @key @roles(read: ["a", "b"])
+}`,
             );
         });
     });
