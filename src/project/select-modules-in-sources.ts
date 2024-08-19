9222fec1788d358f1da0b33600b7bcadb448e782
Jan Melcher <jan.melcher@aeb.com>
2024-08-19 13:25:48 +0200

    fix: fix withModuleSelection discarding project options

--- src/project/select-modules-in-sources.ts
+++ src/project/select-modules-in-sources.ts
@@ -66,7 +66,7 @@ export function selectModulesInProject(
     }
 
     return new Project({
-        ...project,
+        ...project.options,
         sources: newSources,
         modelOptions: {
             ...(project.options.modelOptions ?? {}),
