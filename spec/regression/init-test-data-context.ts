ad2404aacb7411ef431e3e5d31d812e6709d6d13
Jan Melcher <jan.melcher@aeb.com>
2025-10-29 16:13:52 +0100

    test: allow regression suites to have a typescript file to init test data

---
+++ spec/regression/init-test-data-context.ts
@@ -0,0 +1,56 @@
+import { DocumentNode, GraphQLSchema } from 'graphql';
+import { ExecutionResult } from 'graphql';
+import { graphql, print } from 'graphql/index';
+
+export interface ExecuteGraphqlOptions {
+    readonly variables?: { readonly [variable: string]: unknown };
+
+    /**
+     * @default true
+     */
+    readonly assertNoErrors?: boolean;
+
+    readonly authRoles?: ReadonlyArray<string>;
+
+    readonly claims?: { readonly [variable: string]: string | ReadonlyArray<string> };
+}
+
+export class InitTestDataContext {
+    constructor(private readonly schema: GraphQLSchema) {}
+
+    async executeGraphql(
+        document: DocumentNode | string,
+        options: ExecuteGraphqlOptions = {},
+    ): Promise<ExecutionResult> {
+        const assertNoErrors = options.assertNoErrors ?? true;
+        const contextValue = {
+            authRoles: options.authRoles,
+            claims: options.claims,
+        };
+        const source =
+            typeof document === 'string'
+                ? document
+                : (document.loc?.source.body ?? print(document));
+        const operations =
+            typeof document === 'object'
+                ? document.definitions.filter((d) => d.kind === 'OperationDefinition')
+                : [];
+
+        const result = await graphql({
+            schema: this.schema,
+            source,
+            rootValue: {},
+            contextValue,
+            variableValues: options.variables,
+        });
+
+        if (assertNoErrors && result.errors) {
+            const operationName = operations.length === 1 ? operations[0].name?.value : undefined;
+            throw new Error(
+                `GraphQL error${operationName ? ` in ${operationName}` : ''}: ${JSON.stringify(result.errors)}`,
+            );
+        }
+
+        return result;
+    }
+}
