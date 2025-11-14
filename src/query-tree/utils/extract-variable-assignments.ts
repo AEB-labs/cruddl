fd3eed084e71e7154a87ef01d28e6709b681daa7
Jan Melcher <jan.melcher@aeb.com>
2025-11-14 14:07:42 +0100

    perf: hoist root field calculations out of loops to reduce query memory usage
    ArangoDB also has logic to hoist variable assignments if it can, but it sometimes (or rather often in our cases) pushes them down again. This is problematic if the result of the assignment is much smaller than its dependencies. In the case of root entities that are accessed via @root, this is often the case because usually, only a subset of the root's fields are accessed within the loop, but a lot more fields are accessed on root level.
    
    We can prevent ArangoDB from pushing down the variables again by wrapping their assignment in NOEVAL(...)
    
    If a query requests many fields from the root object in general (by default 5), the reduce-extraction-to-projection optimization is no longer applied so the full root object is held in memory, and without variable hosting, can be duplicated for each result item of a traversal, leading to very high memory usage (root entity size * number of collect items)
    
    In the regression test root-fields/root-with-collect in query q, the memory usage increased slightly. This is likely because the added variable assignments have an overhead that is not offset by the hoisting because of the small number of items and small size of objects in the regression tests.

--- src/query-tree/utils/extract-variable-assignments.ts
+++ src/query-tree/utils/extract-variable-assignments.ts
@@ -3,6 +3,7 @@ import {
     ConditionalQueryNode,
     FieldQueryNode,
     FirstOfListQueryNode,
+    HoistableQueryNode,
     ObjectQueryNode,
     OrderClause,
     OrderSpecification,
@@ -11,38 +12,28 @@ import {
     RootEntityIDQueryNode,
     UnaryOperationQueryNode,
     VariableAssignmentQueryNode,
+    VariableQueryNode,
 } from '..';
 
 /**
- * Traverses recursively through Unary/Binary operations, extracts all variable definitions and replaces them by their
- * variable nodes
- * @param {QueryNode} node
- * @param variableAssignmentsList: (will be modified) list to which to add the variable assignment nodes
+ * Traverses recursively through Unary/Binary operations, extracts all variable definitions and
+ * replaces them by their variable nodes
+ *
+ * The variableAssignmentsList variable will be modified to contain all extracted variable
+ * assignments.
  */
 export function extractVariableAssignments(
     node: QueryNode,
     variableAssignmentsList: VariableAssignmentQueryNode[],
 ): QueryNode {
-    if (node instanceof UnaryOperationQueryNode) {
-        return new UnaryOperationQueryNode(
-            extractVariableAssignments(node.valueNode, variableAssignmentsList),
-            node.operator,
-        );
-    }
-    if (node instanceof BinaryOperationQueryNode) {
-        return new BinaryOperationQueryNode(
-            extractVariableAssignments(node.lhs, variableAssignmentsList),
-            node.operator,
-            extractVariableAssignments(node.rhs, variableAssignmentsList),
-        );
-    }
-    if (node instanceof ConditionalQueryNode) {
-        return new ConditionalQueryNode(
-            extractVariableAssignments(node.condition, variableAssignmentsList),
-            extractVariableAssignments(node.expr1, variableAssignmentsList),
-            extractVariableAssignments(node.expr2, variableAssignmentsList),
-        );
-    }
+    // TODO aql-perf: figure out if there are real cases where we should extract variables but we don't
+    // We don't traverse into loops or other more complex nodes at the moment
+    // If we decide to do that in the future, we would need to ensure that we don't extract
+    // variables assignments that depend on loop variables, because they can't be extracted out of
+    // their loop. One way to do this would be to maintain a set of known variables (with a starter
+    // set and newly found assignments), and skip variable assignments that depend on unknown
+    // variables.
+
     if (node instanceof VariableAssignmentQueryNode) {
         // traverse into the variable value node
         const newVariableValueNode = extractVariableAssignments(
@@ -62,22 +53,64 @@ export function extractVariableAssignments(
         }
         return extractVariableAssignments(node.resultNode, variableAssignmentsList);
     }
+
+    if (node instanceof HoistableQueryNode) {
+        // this is basically an "optional" variable assignment - it should be extracted / hoisted
+        // if possible, but otherwise the value will be inline (without variable)
+        const variableNode = new VariableQueryNode(node.variableLabel);
+        const newInnerNode = extractVariableAssignments(node.node, variableAssignmentsList);
+        variableAssignmentsList.push(
+            new VariableAssignmentQueryNode({
+                variableNode,
+                resultNode: variableNode,
+                variableValueNode: newInnerNode,
+            }),
+        );
+        return variableNode;
+    }
+
+    if (node instanceof UnaryOperationQueryNode) {
+        return new UnaryOperationQueryNode(
+            extractVariableAssignments(node.valueNode, variableAssignmentsList),
+            node.operator,
+        );
+    }
+
+    if (node instanceof BinaryOperationQueryNode) {
+        return new BinaryOperationQueryNode(
+            extractVariableAssignments(node.lhs, variableAssignmentsList),
+            node.operator,
+            extractVariableAssignments(node.rhs, variableAssignmentsList),
+        );
+    }
+
+    if (node instanceof ConditionalQueryNode) {
+        return new ConditionalQueryNode(
+            extractVariableAssignments(node.condition, variableAssignmentsList),
+            extractVariableAssignments(node.expr1, variableAssignmentsList),
+            extractVariableAssignments(node.expr2, variableAssignmentsList),
+        );
+    }
+
     if (node instanceof FirstOfListQueryNode) {
         return new FirstOfListQueryNode(
             extractVariableAssignments(node.listNode, variableAssignmentsList),
         );
     }
+
     if (node instanceof FieldQueryNode) {
         return new FieldQueryNode(
             extractVariableAssignments(node.objectNode, variableAssignmentsList),
             node.field,
         );
     }
+
     if (node instanceof RootEntityIDQueryNode) {
         return new RootEntityIDQueryNode(
             extractVariableAssignments(node.objectNode, variableAssignmentsList),
         );
     }
+
     if (node instanceof ObjectQueryNode) {
         return new ObjectQueryNode(
             node.properties.map(
@@ -89,38 +122,6 @@ export function extractVariableAssignments(
             ),
         );
     }
+
     return node;
-}
-
-/**
- * Wraps a queryNode in a list of variable assignments (the logical counterpart to extractVariableAssignments)
- */
-export function prependVariableAssignments(
-    node: QueryNode,
-    variableAssignmentsList: ReadonlyArray<VariableAssignmentQueryNode>,
-) {
-    return variableAssignmentsList.reduce(
-        (currentNode, assignmentNode) =>
-            new VariableAssignmentQueryNode({
-                resultNode: currentNode,
-                variableNode: assignmentNode.variableNode,
-                variableValueNode: assignmentNode.variableValueNode,
-            }),
-        node,
-    );
-}
-
-export function extractVariableAssignmentsInOrderSpecification(
-    orderSpecification: OrderSpecification,
-    variableAssignmentsList: VariableAssignmentQueryNode[],
-): OrderSpecification {
-    return new OrderSpecification(
-        orderSpecification.clauses.map(
-            (clause) =>
-                new OrderClause(
-                    extractVariableAssignments(clause.valueNode, variableAssignmentsList),
-                    clause.direction,
-                ),
-        ),
-    );
 }
--- src/query-tree/utils/index.ts
+++ src/query-tree/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './extract-variable-assignments';
+export * from './referenced-variables';
 export * from './simplify-booleans';
 export * from './static-evaluation';
