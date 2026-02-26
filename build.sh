f4b314e531a0d7809be630c2d482b5ed24a6f501
Jan Melcher <jan.melcher@aeb.com>
2026-02-26 17:51:35 +0100

    chore: switch to vitest, tsx and cjs/esm dual output
    BREAKING CHANGE: the package structure has changed because it now contains both esm (modules) and cjs (commonjs). Deep imports therefore no longer work as they used to. Note that only the entrypoints "cruddl", "cruddl/core", "cruddl/arangodb" and "cruddl/inmemory" are officially supported, and they still work.

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/env bash
 set -euxo pipefail
 rimraf dist
-tsc --skipLibCheck
-cpy --flat src/schema/preparation/source-validation-modules/schema dist/src/schema/preparation/source-validation-modules/schema
-node ./emit-cruddl-version.js
+tsc -p tsconfig.build.cjs.json --skipLibCheck
+tsc -p tsconfig.build.esm.json --skipLibCheck
+printf '{"type":"commonjs"}\n' > dist/cjs/package.json
+printf '{"type":"module"}\n' > dist/esm/package.json
+cpy --flat src/schema/preparation/source-validation-modules/schema dist/cjs/src/schema/preparation/source-validation-modules/schema
+cpy --flat src/schema/preparation/source-validation-modules/schema dist/esm/src/schema/preparation/source-validation-modules/schema
+node ./scripts/emit-cruddl-version.js
 # check e.g. for imports from modules that are not listed as dependencies
 npm run knip
 npm run knip:prod
