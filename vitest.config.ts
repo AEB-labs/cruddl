c3e15051fadd530c23dec144b79839d64989dde9
Jan Melcher <jan.melcher@aeb.com>
2026-02-26 17:51:35 +0100

    chore: switch to vitest, tsx and cjs/esm dual output
    BREAKING CHANGE: the package structure has changed because it now contains both esm (modules) and cjs (commonjs). Deep imports therefore no longer work as they used to. Note that only the entrypoints "cruddl", "cruddl/core", "cruddl/arangodb" and "cruddl/inmemory" are officially supported, and they still work.

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            graphql: 'graphql/index.js',
+        },
+    },
+    test: {
+        environment: 'node',
+        globals: true,
+        isolate: false,
+        fileParallelism: false,
+        silent: 'passed-only',
+        include: ['spec/**/*.spec.ts'],
+        setupFiles: ['./spec/init.vitest.ts'],
+        sequence: {
+            // Custom sorter to run regressions.spec.ts last
+            sorter: (a, b) => {
+                const regression = 'spec/regression/regressions.spec.ts';
+                if (a.file === regression && b.file !== regression) return 1;
+                if (b.file === regression && a.file !== regression) return -1;
+                return a.file.localeCompare(b.file);
+            },
+        },
+    },
+});
