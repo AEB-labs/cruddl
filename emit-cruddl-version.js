549504e029ae3da3d03e62891c39f0546dbc1b27
Jan Melcher <jan.melcher@aeb.com>
2026-02-26 18:21:26 +0100

    build: emit esm versions in the dist/ lib
    They are not exported in the package.json yet.

--- emit-cruddl-version.js
+++ emit-cruddl-version.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const cruddlVersion = require('./package.json').version;
 
 const mockVersion = '0.0.0-local-dev';
-const path = './dist/src/cruddl-version.js';
-const oldContents = fs.readFileSync(path, 'utf8');
-const newContents = oldContents.replace(mockVersion, cruddlVersion);
-fs.writeFileSync('./dist/src/cruddl-version.js', newContents);
+const targets = ['./dist/cjs/src/cruddl-version.js', './dist/esm/src/cruddl-version.js'];
+
+for (const path of targets) {
+    if (!fs.existsSync(path)) {
+        continue;
+    }
+    const oldContents = fs.readFileSync(path, 'utf8');
+    const newContents = oldContents.replace(mockVersion, cruddlVersion);
+    fs.writeFileSync(path, newContents);
+}
