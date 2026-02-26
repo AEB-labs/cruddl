2e4900dd5e4000a82eaf2589b642c036be65dd4a
Jan Melcher <jan.melcher@aeb.com>
2026-02-26 18:21:26 +0100

    build: emit esm versions in the dist/ lib
    They are not exported in the package.json yet.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/env bash
 set -euxo pipefail
 rimraf dist
-tsc --skipLibCheck
-cpy --flat src/schema/preparation/source-validation-modules/schema dist/src/schema/preparation/source-validation-modules/schema
+tsc -p tsconfig.build.cjs.json --skipLibCheck
+tsc -p tsconfig.build.esm.json --skipLibCheck
+cpy --flat src/schema/preparation/source-validation-modules/schema dist/cjs/src/schema/preparation/source-validation-modules/schema
+cpy --flat src/schema/preparation/source-validation-modules/schema dist/esm/src/schema/preparation/source-validation-modules/schema
 node ./emit-cruddl-version.js
 # check e.g. for imports from modules that are not listed as dependencies
 npm run knip
