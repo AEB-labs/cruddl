c41aa6fed3f43ef55aed20d9f937152bf1975052
Jan Melcher <jan.melcher@aeb.com>
2026-03-01 17:37:42 +0100

    refactor: specify extension for deep arangojs imports
    The arangojs package does not have export declarations in the version we are using, so esm mode will only work when the files are specified explicitly.

--- src/database/arangodb/index.ts
+++ src/database/arangodb/index.ts
@@ -1,5 +1,5 @@
-export { CreateCollectionOptions } from 'arangojs/collection';
-export { Config as ArangoJSConfig } from 'arangojs/connection';
+export { CreateCollectionOptions } from 'arangojs/collection.js';
+export { Config as ArangoJSConfig } from 'arangojs/connection.js';
 export { TransactionError } from '../../execution/transaction-error';
 export { ArangoDBAdapter } from './arangodb-adapter';
 export { ArangoDBConfig, KeyGeneratorType } from './config';
