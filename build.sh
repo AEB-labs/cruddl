1a0660f36e8e3cd3a8ecc73589d4971533f3112a
Jan Melcher <jan.melcher@aeb.com>
2026-03-01 20:25:19 +0100

    wip: chore: switch to esm

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ tsc -p tsconfig.build.cjs.json --skipLibCheck
 tsc -p tsconfig.build.esm.json --skipLibCheck
 cpy --flat src/schema/preparation/source-validation-modules/schema dist/cjs/src/schema/preparation/source-validation-modules/schema
 cpy --flat src/schema/preparation/source-validation-modules/schema dist/esm/src/schema/preparation/source-validation-modules/schema
-node ./emit-cruddl-version.js
+node ./scripts/emit-cruddl-version.js
 # check e.g. for imports from modules that are not listed as dependencies
 npm run knip
 npm run knip:prod
--- scripts/emit-cruddl-version.js
+++ scripts/emit-cruddl-version.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
-const cruddlVersion = require('./package.json').version;
+import fs from 'fs';
+import { join } from 'path';
+
+const packageJson = JSON.parse(fs.readFileSync(join(import.meta.dirname, 'package.json'), 'utf8'));
+const cruddlVersion = packageJson.version;
 
 const mockVersion = '0.0.0-local-dev';
 const targets = ['./dist/cjs/src/cruddl-version.js', './dist/esm/src/cruddl-version.js'];
