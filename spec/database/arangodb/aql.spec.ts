040b5d39392ee85353fda9e38f8ae181f7aa8ee9
Jan Melcher <jan.melcher@aeb.com>
2026-03-02 17:49:07 +0100

    refactor: use vitest for assertions instead of chai

--- spec/database/arangodb/aql.spec.ts
+++ spec/database/arangodb/aql.spec.ts
@@ -1,5 +1,4 @@
-import { expect } from 'chai';
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { aql, aqlConfig, AQLVariable } from '../../../src/database/arangodb/aql.js';
 
 describe('aql', () => {
--- spec/init.vitest.ts
+++ spec/init.vitest.ts
@@ -1,9 +1,3 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import deepEqualInAnyOrder from 'deep-equal-in-any-order';
 import colors from '../src/utils/colors.js';
-
-chai.use(chaiAsPromised);
-chai.use(deepEqualInAnyOrder);
 
 colors.enabled = true;
