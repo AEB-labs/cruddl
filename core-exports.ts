32c7d92c76a8925f23850f93c2b20b87d1db1238
Jan Melcher <jan.melcher@aeb.com>
2026-03-03 14:17:54 +0100

    fix: add some missing public exports

--- core-exports.ts
+++ core-exports.ts
@@ -4,9 +4,15 @@ export type {
     ProjectOptions,
     RequestContext,
     RequestProfile,
+    SchemaOptions,
 } from './src/config/interfaces.js';
 export type { Logger, LoggerProvider } from './src/config/logging.js';
-export type { DatabaseAdapter } from './src/database/database-adapter.js';
+export type {
+    DatabaseAdapter,
+    DatabaseAdapterTimings,
+    ExecutionPlan,
+    TransactionStats,
+} from './src/database/database-adapter.js';
 export type {
     Clock,
     ExecutionOptions,
@@ -30,9 +36,18 @@ export {
     applyChangeSet,
     applyYamlAddInMapChange,
 } from './src/model/change-set/apply-change-set.js';
-export { ChangeSet, TextChange, YamlAddInMapChange } from './src/model/change-set/change-set.js';
+export {
+    AppendChange,
+    ChangeSet,
+    TextChange,
+    YamlAddInMapChange,
+} from './src/model/change-set/change-set.js';
 export { TypeKind } from './src/model/config/index.js';
 export * from './src/model/implementation/index.js';
+export { BaseModuleSpecification } from './src/model/implementation/modules/base-module-specification.js';
+export { FieldModuleSpecification } from './src/model/implementation/modules/field-module-specification.js';
+export { ModuleDeclaration } from './src/model/implementation/modules/module-declaration.js';
+export { TypeModuleSpecification } from './src/model/implementation/modules/type-module-specification.js';
 export {
     MessageLocation,
     Model,
@@ -50,4 +65,5 @@ export {
     type SourceConfig,
     type SourceLike,
 } from './src/project/source.js';
+export type { TTLInfo } from './src/project/time-to-live.js';
 export { CORE_SCALARS, DIRECTIVES } from './src/schema/graphql-base.js';
