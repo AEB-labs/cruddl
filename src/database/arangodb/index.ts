d76d584977a671ac2e3cc8d2437df84f7ddab01e
Jan Melcher <jan.melcher@aeb.com>
2026-03-03 14:17:54 +0100

    fix: add some missing public exports

--- src/database/arangodb/index.ts
+++ src/database/arangodb/index.ts
@@ -3,6 +3,7 @@ export type { Config as ArangoJSConfig } from 'arangojs/connection.js';
 export { TransactionError } from '../../execution/transaction-error.js';
 export { ArangoDBAdapter } from './arangodb-adapter.js';
 export type { ArangoDBConfig, KeyGeneratorType } from './config.js';
+export type { ArangoSearchConfiguration } from './schema-migration/arango-search-helpers.js';
 export {
     describeIndex,
     getIndexDescriptor,
