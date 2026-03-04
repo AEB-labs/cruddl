452ca36d612b969751c9cbaa3a99ac83bb0866b2
Jan Melcher <jan.melcher@aeb.com>
2026-03-04 11:14:59 +0100

    test: add more tests for JSON parsing and validation
    Will switch the library for this in the next commit.

--- spec/schema/parsing/parse-json-source.spec.ts
+++ spec/schema/parsing/parse-json-source.spec.ts
@@ -59,38 +59,213 @@ describe('parseJSONSource', () => {
 
     it('reports syntax errors', () => {
         const messages = getMessages(new ProjectSource('test.json', '{"a": \ntrue test'));
-        expect(messages.length).to.equal(1);
-        expect(messages[0].message).to.equal(
-            "Unknown Character 't', expecting a comma or a closing '}'",
-        );
-        expect(JSON.parse(JSON.stringify(messages[0].location))).to.deep.equal({
-            _end: 16,
-            _start: 12,
-            sourceName: 'test.json',
-        });
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character 't', expecting a comma or a closing '}'",
+                location: { sourceName: 'test.json', _start: 12, _end: 16 },
+            },
+        ]);
     });
 
     it('reports syntax errors at the beginning', () => {
         const messages = getMessages(new ProjectSource('test.json', 'abc'));
-        expect(messages.length).to.equal(1);
-        expect(messages[0].message).to.equal(
-            "Unknown character 'a', expecting opening block '{' or '[', or maybe a comment",
-        );
-        expect(JSON.parse(JSON.stringify(messages[0].location))).to.deep.equal({
-            _end: 3,
-            _start: 0,
-            sourceName: 'test.json',
-        });
+        expect(messages).toMatchObject([
+            {
+                message:
+                    "Unknown character 'a', expecting opening block '{' or '[', or maybe a comment",
+                location: { sourceName: 'test.json', _start: 0, _end: 3 },
+            },
+        ]);
     });
 
     it('accepts valid json', () => {
         const messages = getMessages(new ProjectSource('test.json', '{"a": true}'));
-        expect(messages).to.deep.equal([]);
+        expect(messages).toMatchObject([]);
     });
 
-    it('accepts json with comments', () => {
+    it('accepts json with multi line comments', () => {
         const messages = getMessages(new ProjectSource('test.json', '{"a": /* comment */ true}'));
-        expect(messages).to.deep.equal([]);
+        expect(messages).toMatchObject([]);
+    });
+
+    it('accepts json with single line comments', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": \n// comment\n true}'));
+        expect(messages).toMatchObject([]);
+    });
+
+    // behavior of json-lint, not necessarily good
+    it('accepts input with control characters', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a":"\u0001"}'));
+        expect(messages).toMatchObject([]);
+    });
+
+    it('rejects trailing commas in objects and arrays', () => {
+        const objMessages = getMessages(new ProjectSource('test.json', '{"a": 1,}'));
+        expect(objMessages).toMatchObject([
+            {
+                message: "Unknown Character '}', expecting a string for key statement.",
+                location: { sourceName: 'test.json', _start: 8, _end: 9 },
+            },
+        ]);
+        const arrMessages = getMessages(new ProjectSource('test.json', '[1,2,]'));
+        expect(arrMessages).toMatchObject([
+            {
+                message: 'Unexpected End Of Array Error. Expecting a value statement.',
+                location: { sourceName: 'test.json', _start: 5, _end: 6 },
+            },
+        ]);
+    });
+
+    it('rejects unterminated multi-line comments', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{/* unterminated "a": 1}'));
+        expect(messages).toMatchObject([
+            {
+                message: "EOF Error, expecting closing '}'.",
+                location: { sourceName: 'test.json', _start: 25, _end: 24 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an invalid symbol at the beginning', () => {
+        const messages = getMessages(new ProjectSource('test.json', 'abc'));
+        expect(messages).toMatchObject([
+            {
+                message:
+                    "Unknown character 'a', expecting opening block '{' or '[', or maybe a comment",
+                location: { sourceName: 'test.json', _start: 0, _end: 3 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an invalid symbol in an object', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": true test}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character 't', expecting a comma or a closing '}'",
+                location: { sourceName: 'test.json', _start: 11, _end: 16 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a property name is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{1: 2}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character '1', expecting a string for key statement.",
+                location: { sourceName: 'test.json', _start: 1, _end: 6 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a value is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": }'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character '}', expecting a value.",
+                location: { sourceName: 'test.json', _start: 6, _end: 7 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a colon is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a" 1}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character '1', expecting a semicolon.",
+                location: { sourceName: 'test.json', _start: 5, _end: 7 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a comma is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": 1 "b":2}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character '\"', expecting a comma or a closing '}'",
+                location: { sourceName: 'test.json', _start: 8, _end: 14 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a closing brace is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a":1'));
+        expect(messages).toMatchObject([
+            {
+                message: "EOF Error, expecting closing '}'.",
+                location: { sourceName: 'test.json', _start: 7, _end: 6 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when a closing bracket is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', '[1,2'));
+        expect(messages).toMatchObject([
+            {
+                message: "EOF Error. Expecting closing ']'",
+                location: { sourceName: 'test.json', _start: 4, _end: 4 },
+            },
+        ]);
+    });
+
+    it('reports malformed input when end of file is expected', () => {
+        const messages = getMessages(new ProjectSource('test.json', 'true false'));
+        expect(messages).toMatchObject([
+            {
+                message:
+                    "Unknown character 't', expecting opening block '{' or '[', or maybe a comment",
+                location: { sourceName: 'test.json', _start: 0, _end: 10 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an unexpected end of comment', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": /* x '));
+        expect(messages).toMatchObject([
+            {
+                message: "EOF Error, expecting closing '}'.",
+                location: { sourceName: 'test.json', _start: 13, _end: 11 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an unexpected end of string', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": "x'));
+        expect(messages).toMatchObject([
+            {
+                message: `EOF: No close string '"' found.`,
+                location: { sourceName: 'test.json', _start: 8, _end: 8 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an unexpected end of number', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a": 1e'));
+        expect(messages).toMatchObject([
+            {
+                message: "Unknown Character 'e', expecting a comma or a closing '}'",
+                location: { sourceName: 'test.json', _start: 7, _end: 8 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with invalid unicode', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a":"\\u12G4"}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Invalid Reverse Solidus '\\' declaration.",
+                location: { sourceName: 'test.json', _start: 6, _end: 14 },
+            },
+        ]);
+    });
+
+    it('reports malformed input with an invalid escape character', () => {
+        const messages = getMessages(new ProjectSource('test.json', '{"a":"\\x"}'));
+        expect(messages).toMatchObject([
+            {
+                message: "Invalid Reverse Solidus '\\' declaration.",
+                location: { sourceName: 'test.json', _start: 6, _end: 10 },
+            },
+        ]);
     });
 });
 
