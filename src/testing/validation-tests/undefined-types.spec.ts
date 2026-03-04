9f09ac768fa6cbb1d0dd03fe70ceb71095efca02
Jan Melcher <jan.melcher@aeb.com>
2026-03-04 16:22:34 +0100

    refactor: move tests from spec/ dir into the src/ directories
    Many directories in spec/ mirrored the src/ directory, so it makes sense to put these files directly next to the source files.
    
    Having a single rootDir for typescript files simplifies the project and avoids a src/ subdir in the dist/ directory.

---
+++ src/testing/validation-tests/undefined-types.spec.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest';
+import {
+    assertValidatorAcceptsAndDoesNotWarn,
+    validate,
+} from '../utils/source-validation-utils.js';
+
+const invalidSource = `
+            type Test @rootEntity {
+                other: Other2
+                date: DateTime
+            }
+            
+            type Other @rootEntity {
+                field: String2
+            }
+        `;
+
+const validSource = `
+            type Test @rootEntity {
+                other: Other @reference(keyField: "otherKey")
+                otherKey: String
+                date: DateTime
+            }
+            
+            type Other @rootEntity {
+                field: String @key
+            }
+        `;
+
+describe('undefined-types validator', () => {
+    it('points out missing types', () => {
+        const validationResult = validate(invalidSource);
+        expect(validationResult.hasErrors()).to.be.true;
+        expect(validationResult.messages.length).to.equal(2);
+        expect(validationResult.messages[0].message).to.equal('Type "Other2" not found.');
+        expect(validationResult.messages[1].message).to.equal('Type "String2" not found.');
+    });
+
+    it('accepts valid sources', () => {
+        assertValidatorAcceptsAndDoesNotWarn(validSource);
+    });
+});
